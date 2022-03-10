12ffb1f7f743bcf9cb3921e32bd2fc10b9fafd20
Israel Sunuwar <isunuwar@inctic.io>
2022-03-10 16:30:33 +0545

    bug fixes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,98 +1,98 @@
-'use strict';
+// 'use strict';
 
-let  icon = document.querySelector(".fa-moon");
-let  logo = document.querySelector(".is-logo");
-let darkMode = localStorage.getItem('darkMode');
+// let  icon = document.querySelector(".fa-moon");
+// let  logo = document.querySelector(".is-logo");
+// let darkMode = localStorage.getItem('darkMode');
 
-// check for emabling dark darkMode
-const enableDarkMode = () => {
-  document.body.classList.add("dark-theme");
-  // toggle image and icon
-  logo.src = "./assets/images/logo-light.png";
-  icon.className = 'fa fa-circle';
-  localStorage.setItem('darkMode', "enabled");
-}
+// // check for emabling dark darkMode
+// const enableDarkMode = () => {
+//   document.body.classList.add("dark-theme");
+//   // toggle image and icon
+//   logo.src = "./assets/images/logo-light.png";
+//   icon.className = 'fa fa-circle';
+//   localStorage.setItem('darkMode', "enabled");
+// }
 
-const disableDarkMode = () => {
-  // toggle image and icon
-  document.body.classList.remove("dark-theme");
-  logo.src = "./assets/images/logo.png";
-  icon.className = 'fa fa-moon';
-  localStorage.setItem('darkMode', null);
-}
+// const disableDarkMode = () => {
+//   // toggle image and icon
+//   document.body.classList.remove("dark-theme");
+//   logo.src = "./assets/images/logo.png";
+//   icon.className = 'fa fa-moon';
+//   localStorage.setItem('darkMode', null);
+// }
 
-// calling when the icon is clicked on web format
-// icon.addEventListener('click', function(){
-//   // updating before checking darkMode
-//   darkMode = localStorage.getItem('darkMode');
-//   if (darkMode !== "enabled") {
-//     enableDarkMode();
-//   }
-//   else {
-//     disableDarkMode();
-//   }
-// });
+// // calling when the icon is clicked on web format
+// // icon.addEventListener('click', function(){
+// //   // updating before checking darkMode
+// //   darkMode = localStorage.getItem('darkMode');
+// //   if (darkMode !== "enabled") {
+// //     enableDarkMode();
+// //   }
+// //   else {
+// //     disableDarkMode();
+// //   }
+// // });
 
-// checking if the theme browser is switched or not
-if (darkMode === 'enabled') {
-  enableDarkMode();
-}
+// // checking if the theme browser is switched or not
+// if (darkMode === 'enabled') {
+//   enableDarkMode();
+// }
 
-// for mbl mblIcon
-let  mblIcon = document.querySelector(".mbl-moon");
-let  mblLogo = document.querySelector(".mbl-is-logo");
+// // for mbl mblIcon
+// let  mblIcon = document.querySelector(".mbl-moon");
+// let  mblLogo = document.querySelector(".mbl-is-logo");
 
-const enableMblDarkMode = () => {
-  document.body.classList.add("dark-theme");
-  // toggle image and icon
-  mblLogo.src = "./assets/images/logo-light.png";
-  mblIcon.className = 'fa fa-circle';
-  localStorage.setItem('darkMode', "enabled");
-}
+// const enableMblDarkMode = () => {
+//   document.body.classList.add("dark-theme");
+//   // toggle image and icon
+//   mblLogo.src = "./assets/images/logo-light.png";
+//   mblIcon.className = 'fa fa-circle';
+//   localStorage.setItem('darkMode', "enabled");
+// }
 
-const disableMblDarkMode = () => {
-  // toggle image and icon
-  document.body.classList.remove("dark-theme");
-  mblLogo.src = "./assets/images/logo.png";
-  mblIcon.className = 'fa fa-moon';
-  localStorage.setItem('darkMode', null);
-}
+// const disableMblDarkMode = () => {
+//   // toggle image and icon
+//   document.body.classList.remove("dark-theme");
+//   mblLogo.src = "./assets/images/logo.png";
+//   mblIcon.className = 'fa fa-moon';
+//   localStorage.setItem('darkMode', null);
+// }
 
-// calling when the icon is clicked on responsive format
-// mblIcon.addEventListener('click', function(){
-//   // updating before checking darkMode
-//   darkMode = localStorage.getItem('darkMode');
-//   if (darkMode !== "enabled") {
-//     enableMblDarkMode();
-//   }
-//   else {
-//     disableMblDarkMode();
-//   }
-// });
+// // calling when the icon is clicked on responsive format
+// // mblIcon.addEventListener('click', function(){
+// //   // updating before checking darkMode
+// //   darkMode = localStorage.getItem('darkMode');
+// //   if (darkMode !== "enabled") {
+// //     enableMblDarkMode();
+// //   }
+// //   else {
+// //     disableMblDarkMode();
+// //   }
+// // });
 
-// responsive navigation
-let burger = document.querySelector('.burger');
-let nav = document.querySelector('.is-navbar-wrapper');
-let navLinks = document.querySelectorAll('.is-navbar-wrapper ul li');
+// // responsive navigation
+// let burger = document.querySelector('.burger');
+// let nav = document.querySelector('.is-navbar-wrapper');
+// let navLinks = document.querySelectorAll('.is-navbar-wrapper ul li');
 
-const toggleNav = function() {
-  nav.classList.toggle('active');
-  document.body.classList.toggle("fixed-position");
-  // burger animation
-  burger.classList.toggle('active');
-}
+// const toggleNav = function() {
+//   nav.classList.toggle('active');
+//   document.body.classList.toggle("fixed-position");
+//   // burger animation
+//   burger.classList.toggle('active');
+// }
 
-const navSlide = () => {
-  burger.addEventListener('click', toggleNav);
-}
+// const navSlide = () => {
+//   burger.addEventListener('click', toggleNav);
+// }
 
-const navClick = () => {
-  navLinks.forEach((navLink) => {
-    navLink.addEventListener('click', toggleNav);
-  });
-}
+// const navClick = () => {
+//   navLinks.forEach((navLink) => {
+//     navLink.addEventListener('click', toggleNav);
+//   });
+// }
 
-function callWhenClicked() {
-  alert('its here yayyy');
-  // navClick(), navSlide() 
-}
+// function callWhenClicked() {
+//   alert('its here yayyy');
+//   // navClick(), navSlide() 
+// }
