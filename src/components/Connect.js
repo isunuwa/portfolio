e9b3f5479d1a568e876989382011e51637d7ed3b
israel sunuwar <israelsunuwar.com>
2022-08-25 21:09:04 +0545

    bug fixes and ui updated

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Connect = () => {
   return (
-    <section class="hero-connect-section">
-      <div class="is-connect-wrapper">
-        <div class="connect-div" id="contact">
-          <div class="is-connect-desc">
-            <h2 class="hero-heading">Connect with me</h2>
+    <section className="hero-connect-section">
+      <div className="is-connect-wrapper">
+        <div className="connect-div" id="contact">
+          <div className="is-connect-desc">
+            <h2 className="hero-heading">Connect with me</h2>
             <p>We could be a match made to a company grow stronger. If you have any questions, or just want to connect feel free to reach me.</p>
-            <a href="mailto:israelsunuwar@gmail.com" target="_blank" rel="noopener noreferrer" class="is-highlight">israelsunuwar@gmail.com</a>
+            <a href="mailto:israelsunuwar@gmail.com" target="_blank" rel="noopener noreferrer" className="is-highlight">israelsunuwar@gmail.com</a>
           </div>
         </div>
       </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -17,9 +17,10 @@ const Intro = () => {
           <p>
             I design ethical, accessible websites for brands using tech for good.
           </p>
+          <p className='alert-msg'> WIP </p>
 
           <div className='is-intro__mail'>
-            <a href="mailto:israelsunuwar@gmail.com" class="button in-btn-lg w-button">israelsunuwar@gmail.com</a>
+            <a href="mailto:israelsunuwar@gmail.com" className="button in-btn-lg w-button">israelsunuwar@gmail.com</a>
           </div>
         </div>
       </div>
